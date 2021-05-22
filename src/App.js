884c4e25e81d29b10f0e883f42dfdd1f022eb4a4
George Thomas <gcthomas@uw.edu>
2021-05-21 21:23:23 -0700

    Fetch monster list from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +6,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { 
-          name: 'Frankenstein',
-          id: 'str1'
-        },
-        { 
-          name: 'Dracula',
-          id: 'str2'        },
-        { 
-          name: 'Zombie',
-          id: 'str3'
-        },       
-      ]
+      monsters: []
     };
   }
+
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
 
   render(){
     return (
